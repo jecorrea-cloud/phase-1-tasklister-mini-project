0bb2c4e0ac0d422e8a00122c5f35bb67f51f4c8f
joaquin <jecorrea@protonmail.com>
2021-04-30 02:27:38 -0400

    incomplete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+let createTask = document.getElementById('Create-Task-Form');
+
+function preventForm(){
+  createTask.preventDefault();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  preventForm();
 });
