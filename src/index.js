8235d335446b3411864c712ee4de54dd4c704c96
joaquin <jecorrea@protonmail.com>
2021-05-14 11:11:00 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-let createTask = document.getElementById('Create-Task-Form');
+//Global variables
+//Grabbing the form where user inputs data and the list where that data will be appended
+let taskForm = document.querySelector("form#create-task-form"),
+taskList = document.querySelector("ul#tasks");
 
-function preventForm(){
-  createTask.preventDefault();
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  preventForm();
+//Calling an event on the form
+taskForm.addEventListener("submit",(e)=>{
+  //Preventing the page-reload default behavior of <form>
+  e.preventDefault();
+  
+  //Grabbing the form's input text and assigning it to a variable
+  let whatUserTyped = e.target['new-task-description'].value;
+
+  //Invoking addElementToList() and passing our new value
+  addElementToList(whatUserTyped);
+
+  //This line cleans up whatever was type
+   e.target.reset();
 });
+
+
+//addElementToList() creates a list node with a text param, adds a delete
+// button and appends it to the global <ul>
+function addElementToList(newTask){
+  let liNode = document.createElement("LI")
+  liNode.textContent = newTask
+  taskList.append(liNode)
+}
